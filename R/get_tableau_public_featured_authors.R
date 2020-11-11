24940158cccd964f88af564db1658e472e52e3a6
Will Sutton <sutton_999@hotmail.com>
2020-11-11 21:09:46 +0000

    adding wokflow for favourites to a feed

--- R/get_tableau_public_featured_authors.R
+++ R/get_tableau_public_featured_authors.R
@@ -6,11 +6,14 @@ source('functions/function_get_tableau_public_api_extract.R')
 source('functions/function_get_last_n_tableau_vizs_extract.R')
 source('functions/function_get_tableau_viz_screenshot_url.R')
 source('functions/function_get_tableau_viz_thumbnail_url.R')
+source('functions/function_get_workbook_details.R')
+source('functions/function_get_profile_favourites.R')
 source('functions/function_get_featured_authors.R')
 
 featured_authors_df <- get_featured_authors()
 featured_authors <- featured_authors_df$profile_name
 
+# Get Feature Authors latest vizzes
 for(i in 1:length(featured_authors)){
   featured_df <- get_last_n_tableau_vizs_extract(featured_authors[i],20)
   
@@ -44,3 +47,34 @@ for(i in 1:length(featured_authors)){
 featured_output$last_publish_datetime <- as.POSIXct(featured_output$last_publish/1000, origin="1970-01-01")
 featured_output <- arrange(featured_output,-last_publish)
 write.csv(featured_output,"data/featured_authors_feed.csv",row.names = F)
+
+for(i in 1:length(featured_authors)){
+  favourited <- get_profile_favourites(featured_authors[i])[1:20,]
+  
+  if(i == 1){
+    favourite_df <- favourited
+  }
+  if(i != 1){
+    favourite_df <- c(favourite_df,favourited)
+  }
+}
+
+favourite_workbooks <- unique(favourite_df[!is.na(favourite_df)])
+
+for(i in 1:length(favourite_workbooks)){
+  fav <- get_workbook_details(favourite_workbooks[i])
+  
+  if(i == 1){
+    fav_df <- fav
+  }
+  if(i != 1){
+    fav_df <- rbind(fav_df,fav)
+  }
+}
+visible_favs <- filter(fav_df,showInProfile==TRUE)
+
+fav_workbooks <- paste0('https://public.tableau.com/profile/',visible_favs$authorProfileName,'#!/vizhome/',gsub('/sheets/','/',visible_favs$defaultViewRepoUrl))
+#workbooks_last_publish <- visible_workbooks$lastPublishDate
+
+
+
