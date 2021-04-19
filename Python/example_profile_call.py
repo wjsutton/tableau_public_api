6a488f8fb970d13fc48459252eb463d77dd3f546
Will Sutton <sutton_999@hotmail.com>
2021-04-19 21:36:13 +0100

    updating profile api call

--- Python/example_profile_call.py
+++ Python/example_profile_call.py
@@ -1,3 +1,8 @@
+# TO DO 
+# Parse out profile address data fields
+# can take options: country, state, city but each can also be empty but not quoted
+
+
 import json
 import urllib3
 import pandas as pd
@@ -13,21 +18,29 @@ profile = pd.json_normalize(tableau_profile, max_level=0)
 #address = pd.json_normalize(tableau_profile['address'], max_level=0)
 websites = pd.json_normalize(tableau_profile['websites'], max_level=0)
 workbooks = pd.json_normalize(tableau_profile['workbooks'], max_level=0)
-#loc.workbooks
 
-attributions = workbooks[workbooks['attributions'].str.len() > 0] #.reset_index()
-#workbook_attributions = pd.DataFrame()
-#if len(attributions)>0:
-print(attributions)
-#workbook_attributions = pd.json_normalize(attributions['attributions'], max_level=0)
-#print(workbook_attributions)
-#workbook_attributions = pd.json_normalize(workbooks['attributions'], max_level=0)
-#del profile['address']
+
+# Finding attributions and merging to workbooks
+attributions_df = []
+for i in workbooks.index:
+    attributions = pd.json_normalize(workbooks['attributions'][i])
+    if len(attributions) > 0:
+        attributions.columns = 'attribution_' + attributions.columns
+        attributions['workbookRepoUrl'] = workbooks['workbookRepoUrl'][i]
+        attributions_df.append(attributions)
+
+if len(attributions_df) > 0:
+    attributions_df = pd.concat(attributions_df)
+    workbooks = pd.merge(workbooks,attributions_df, on='workbookRepoUrl', how='left')
+
 del profile['websites']
 del profile['workbooks']
 
-#print(profile)
-#print(websites)
-#print(workbooks)
+print(workbooks)
 
-#print(workbook_attributions)
+print(profile)
+print(tableau_profile['address'])
+#address = pd.json_normalize(tableau_profile['address'])
+#print(address)
+#print(tableau_profile['address']['state'])
+#print(tableau_profile['address']['city'])
